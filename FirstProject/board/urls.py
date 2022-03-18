57e68db35278fb3fc523f66bb550d75bf8565816
dawoon <dawoon_jung@mycelebs.com>
2022-03-18 17:10:30 +0900

    Fix: 완료. 웹페이징 부분 이상한데 코드상 문제없음..

--- FirstProject/board/urls.py
+++ FirstProject/board/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.board_list, name='board_list'),
     path('write/', views.board_write, name='board_write'),
     path('detail/<int:pk>', views.board_detail, name='board_detail'),
-    path('detail/<int:pk>/delete/', views.board_delete, name='board_delete')
+    path('detail/<int:pk>/delete/', views.board_delete, name='board_delete'),
+    path('detail/<int:pk>/modify/', views.board_modify, name='board_modify')
 ]
